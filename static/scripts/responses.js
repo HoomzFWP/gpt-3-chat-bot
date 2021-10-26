43ce00237a853c5d2f4c98d1d2e3bdfa1437f4f8
hoomzfwp <ndamte@gmail.com>
2021-10-26 09:18:58 -0500

    initial commit

---
+++ static/scripts/responses.js
@@ -0,0 +1,24 @@
+function getBotResponse(input) {
+    // TODO: I am going to plop the worlds 
+    // most powerful natural language processing 
+    // model right here :)
+
+
+    //rock paper scissors
+    if (input == "rock") {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }
+}
